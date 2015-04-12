ffe38359e7255df362a9e34cde717a1c20432aeb
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-04-13 00:27:14 +0200

    Initial commit

--- src/JFET.jl
+++ src/JFET.jl
@@ -1,5 +1,13 @@
 module JFET
 
-# package code goes here
+using FastGaussQuadrature
+
+export ReferenceLine, ReferenceTriangle, ReferenceSquare,
+       ReferenceTetrahedron, ReferenceCube
+
+export dim, volume, make_quadrule, integrate, points, weights
+
+include("refelements.jl")
+include("quadrature.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using JFET
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+include("test_refelements.jl")
